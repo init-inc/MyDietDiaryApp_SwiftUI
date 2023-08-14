5e6ea9b1a6e4f6fe47e96a4546955b34f68d038b
naoki.takehisa <naoki.takehisa@init-inc.com>
2023-08-14 09:07:44 +0900

    Initial Commit

---
+++ MyDietDiaryApp_SwiftUI/MyDietDiaryApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDietDiaryApp_SwiftUIApp.swift
+//  MyDietDiaryApp_SwiftUI
+//
+//  Created by 武久 なおき on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MyDietDiaryApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
