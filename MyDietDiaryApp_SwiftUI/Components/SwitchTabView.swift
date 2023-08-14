5913fda4423276c38e0119ddf83c29aa391c1206
naoki.takehisa <naoki.takehisa@init-inc.com>
2023-08-14 09:55:24 +0900

    READMEファイル、Chapter2までの実装

---
+++ MyDietDiaryApp_SwiftUI/Components/SwitchTabView.swift
@@ -0,0 +1,20 @@
+//
+//  SwitchTabView.swift
+//  MyDietDiaryApp_SwiftUI
+//
+//  Created by 武久 なおき on 2023/08/14.
+//
+
+import SwiftUI
+
+struct SwitchTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwitchTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwitchTabView()
+    }
+}
